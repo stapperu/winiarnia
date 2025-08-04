6ec30947d06ccde0fa68577fb349834d989cb392
stapperu <k.kubiak94@gmail.com>
2025-08-04 21:11:28 +0200

    big upgrade, we got them maps!

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,15 @@ const contactBtn = document.querySelector(".aboutus")
 const modalPhoto = document.querySelector('.modal-photo')
 const modalText = document.querySelector('.modal-text')
 const modalH1 = document.querySelector('.modal-h1')
-// const x = document.querySelector('')
+const locationPopup = document.querySelector('.location-popup')
+const addressBtn=document.querySelector(".addressBtn")
 
-function fillModal(modalPhoto,modalH1,modalText) {
+function fillModal(modalPhoto,modalH1,modalText) {}
 
+function openLocationPopup(){
+    locationPopup.classList.toggle("show-map");
 }
-console.log(aboutusBtn);
+
+
 aboutusBtn.addEventListener("click", ()=>{console.log("navclicked");})
+addressBtn.addEventListener("click",openLocationPopup)
