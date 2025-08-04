bcef14ac305694d2225803750f6a9482f736683d
stapperu <k.kubiak94@gmail.com>
2025-08-04 19:53:27 +0200

    commit from pc, reworked design, some keyframes animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
+const aboutusBtn = document.querySelector(".aboutus")
+const prizesBtn = document.querySelector(".prizes")
+const locationsBtn = document.querySelector(".aboutus")
+const contactBtn = document.querySelector(".aboutus")
+const modalPhoto = document.querySelector('.modal-photo')
+const modalText = document.querySelector('.modal-text')
+const modalH1 = document.querySelector('.modal-h1')
 // const x = document.querySelector('')
-// const x = document.querySelector('')
-// const x = document.querySelector('')
-// const x = document.querySelector('')
-// const x = document.querySelector('')
+
+function fillModal(modalPhoto,modalH1,modalText) {
+
+}
+console.log(aboutusBtn);
+aboutusBtn.addEventListener("click", ()=>{console.log("navclicked");})
